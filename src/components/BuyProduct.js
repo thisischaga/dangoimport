f3e727cf5fcec456ee927e6141e225bd346cff3f
thisischaga <redocstar@gmail.com>
2025-09-11 19:37:00 +0200

    Nouvelle mise à jour pour la version finale*

--- src/components/BuyProduct.js
+++ src/components/BuyProduct.js
@@ -43,7 +43,7 @@ const BuyProduct = ({image, name, price, description, isVisibled})=>{
         window.location.reload();
     };
     
-    const toTheOtp = ()=>{
+    const toTheOtp = async ()=>{
         alert('OTP pas encore prêt !')
         if (!userName || !userNumber || !productQuantity || !userPref || !selectedCountry) {
             alert('Veuillez remplir tous les champs !');
@@ -52,13 +52,27 @@ const BuyProduct = ({image, name, price, description, isVisibled})=>{
         } else if (userNumber.length < 8) {
             alert('Veillez entrer un numéro de téléphone valide !')
         }
+        else{
+            try {
+                const res = await axios.post('https://dangoimport-server.onrender.com/api/send-otp', { userNumber });
+                if (res.data.message === 'OTP envoyé avec succès') {
+                    setOtpSystem(true);
+                }
+                  setBackendMessage(res.data.message);
+            } catch (err) {
+              setBackendMessage('Erreur lors de l’envoi de l’OTP');
+              setIsError(true);
+            } finally {
+                setIsLoading(false);
+            }
+        };
     }
     const handleSubmit = async (e) => {
         e.preventDefault();
         
         setIsLoading(true);
         try {
-        const otpResponse = await axios.post('https://dangoimport-server.onrender.com/api/verify-otp', {
+        const otpResponse = await axios.post('https://dangoimport-server.onrender.com/api/verify-smsotp', {
             userNumber,
             otp,
         });
@@ -99,78 +113,77 @@ const BuyProduct = ({image, name, price, description, isVisibled})=>{
             
             <main>
                 <div className={styles.buyingForm}>
-                    <button className={styles.hiddenBtn} onClick={hiddeForm}>Retourner</button>
+                    <button className={styles.hiddenBtn} onClick={hiddeForm}>Annuler</button>
                     <div className={styles.title}>
                         <h1>Acheter</h1>
                     </div>
-                    <div className={styles.metaData}>
-                        <h2>{name} </h2>
-                        <h2>Prix : <span>{price} fcfa</span></h2>
-                    </div>
-                    <div className={styles.form}>
-                        
-                        <div className={styles.productDes}>
-                            <img src={image} alt='productImg'/>
-                            <p>
-                                {description}
-                            </p>
+                    <div className={otpSystem === true? 'hidden':''}>
+                        <div className={styles.metaData}>
+                            <h2>{name} </h2>
+                            <h2>Prix : <span>{price} fcfa</span></h2>
                         </div>
-                        <div>
-                            <label>Nom <span>*</span></label><br/>
-                            <input type='text' placeholder='Entrez votre nom...'  onChange={handleUserNameChange} value={userName}/><br/>
+                        <div className={styles.form}>
                             
-                            <label>Préférence <span>*</span></label><br/>
-                            <textArea type='text' placeholder='Ajouter des préférences pour ce produit...'  onChange={handlePrefChange} value={userPref}/><br/>
-                            
-                            <label>Téléphone <span>*</span></label><br/>
-                            <input type='number' placeholder='Numéro de téléphone...'  onChange={handleUserNumberChange} value={userNumber}/><br/>
-                            <label>Quantité <span>*</span></label><br/>
-                            <input type='number' placeholder='Quantité...'  onChange={handlePqChange} value={productQuantity}/><br/>
-                            <div > 
-                                <div className={styles.radios}> 
-                                    <div className={styles.radiosContainer}> 
-                                        <input type='radio' className={styles.radioInput} value={benin} checked={selectedCountry === benin} onChange={handleCountryChange}/> 
-                                        <label>Bénin </label><br/> 
-                                    </div> 
-                                    <div className={styles.radiosContainer}> 
-                                        <input type='radio' className={styles.radioInput} value={togo} checked={selectedCountry === togo} onChange={handleCountryChange} /> 
-                                        <label>Togo</label><br/> 
-                                    </div> 
-                                </div> 
+                            <div className={styles.productDes}>
+                                <img src={image} alt='productImg'/>
+                                <p>
+                                    {description}
+                                </p>
                             </div>
-                            
-                            
-                            {/* OTP */}
-                                {otpSystem && (
-                                <div className={styles.finalStep}>
-                                    <p>Un code a été envoyé à {userNumber}</p>
-                                    <p style={{textAlign: 'center'}}><input type='number' maxLength={6} placeholder='* * * * * *' style={{padding: '10px', width: '160px', 
-                                        backgroundColor: 'transparent', outline:'none', border: 'none', borderBottom: '3px solid rgb(36, 123, 181)', 
-                                        color: 'white', fontWeight:'bold', letterSpacing: '10px', fontSize: '20px', textAlign: 'center'}} value={otp} onChange={handleOtpChange} required/></p>
-                                    <p>
-                                        <input type='checkbox' checked={checked} onChange={toggleCheck} />
-                                        J'ai lu et j’accepte les <a href='/cgu'>conditions générales d'utilisation</a>
-                                    </p>
-                                    {checked && (
-                                        <button className={styles.btnSubmit} onClick={handleSubmit}>
-                                          {isLoading ? 'Patientez...' : 'CONFIRMER'}
+                            <div>
+                                <label>Nom <span>*</span></label><br/>
+                                <input type='text' placeholder='Entrez votre nom...'  onChange={handleUserNameChange} value={userName}/><br/>
+                                
+                                <label>Préférence <span>*</span></label><br/>
+                                <textArea type='text' placeholder='Ajouter des préférences pour ce produit...'  onChange={handlePrefChange} value={userPref}/><br/>
+                                
+                                <label>Téléphone <span>*</span></label><br/>
+                                <input type='number' placeholder='EX: +22899152036'  onChange={handleUserNumberChange} value={userNumber}/><br/>
+                                <label>Quantité <span>*</span></label><br/>
+                                <input type='number' placeholder='Quantité...'  onChange={handlePqChange} value={productQuantity}/><br/>
+                                <div > 
+                                    <div className={styles.radios}> 
+                                        <div className={styles.radiosContainer}> 
+                                            <input type='radio' className={styles.radioInput} value={benin} checked={selectedCountry === benin} onChange={handleCountryChange}/> 
+                                            <label>Bénin </label><br/> 
+                                        </div> 
+                                        <div className={styles.radiosContainer}> 
+                                            <input type='radio' className={styles.radioInput} value={togo} checked={selectedCountry === togo} onChange={handleCountryChange} /> 
+                                            <label>Togo</label><br/> 
+                                        </div> 
+                                        <button className={styles.btnSubmit} onClick={toTheOtp}>
+                                            {isLoading ? 'Patientez...' : 'CONFIRMER'}
                                         </button>
-                                      )}
-                                    </div>
-                                    )}
-                                  {messageBoxIs && (
-                                    <div className={styles.messageBox}>
-                                      {isSuccess && <FaCheckCircle size={40} color='green' />}
-                                      {isError && <FaTimesCircle size={40} color='red' />}
-                                      <h3>{backendMessage}</h3>
-                                    </div>
-                                  )}
+                                    </div> 
+                                </div>
+                            </div>
                         </div>
-                        
-                        <button className={styles.btnSubmit} onClick={toTheOtp}>
-                            {isLoading ? 'Patientez...' : 'CONFIRMER'}
-                        </button>
-                        
+                    </div>
+                    <div>
+                         {otpSystem && (
+                            <div className={styles.finalStep}>
+                                <p>Un code a été envoyé à {userNumber}</p>
+                                <p style={{textAlign: 'center'}}><input type='number' maxLength={6} placeholder='* * * * * *' style={{padding: '10px', width: '160px', 
+                                    backgroundColor: 'transparent', outline:'none', border: 'none', borderBottom: '3px solid rgb(36, 123, 181)', 
+                                    color: 'white', fontWeight:'bold', letterSpacing: '10px', fontSize: '20px', textAlign: 'center'}} value={otp} onChange={handleOtpChange} required/></p>
+                                <p>
+                                <input type='checkbox' checked={checked} onChange={toggleCheck} />
+                                    J'ai lu et j’accepte les <a href='/cgu'>conditions générales d'utilisation</a>
+                                </p>
+                                {checked && (
+                                    <button className={styles.btnSubmit} onClick={handleSubmit}>
+                                      {isLoading ? 'Patientez...' : 'CONFIRMER'}
+                                    </button>
+                                )}
+                            </div>
+                        )}
+                        {messageBoxIs && (
+                        <div className={styles.messageBox}>
+                            {isSuccess && <FaCheckCircle size={40} color='green' />}
+                            {isError && <FaTimesCircle size={40} color='red' />}
+                            <h3>{backendMessage}</h3>
+                        </div>
+                        )}
                     </div>
                 </div>
             </main>
@@ -179,3 +192,4 @@ const BuyProduct = ({image, name, price, description, isVisibled})=>{
 }
 
 export default BuyProduct;
+
